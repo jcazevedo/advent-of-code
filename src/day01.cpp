48adb3824dc2f4b90e997528c3ad72ff3d770c73
Joao Azevedo <joao.c.azevedo@gmail.com>
2019-12-01 11:32:48 +0000

    Add solution for second part of day 1

--- src/day01.cpp
+++ src/day01.cpp
@@ -2,12 +2,37 @@
 
 using namespace std;
 
+long long getFuelPart1(const vector<long long> &masses) {
+  long long tot = 0;
+  for (long long mass : masses)
+    tot += (mass / 3) - 2;
+  return tot;
+}
+
+long long getModuleFuel(long long mass, map<long long, long long>& cache) {
+  if (cache.find(mass) == cache.end()) {
+    long long next = max(mass / 3 - 2, 0ll);
+    long long fuel = next + (next ? getModuleFuel(next, cache) : 0);
+    cache[mass] = fuel;
+  }
+  return cache[mass];
+}
+
+long long getFuelPart2(const vector<long long> &masses) {
+  map<long long, long long> cache;
+  long long tot = 0;
+  for (long long mass : masses)
+    tot += getModuleFuel(mass, cache);
+  return tot;
+}
+
 int main() {
   ifstream fin("day01.in");
-  long long tot = 0, v;
-  while (fin >> v) {
-    tot += (v / 3) - 2;
-  }
-  cout << "Part 1: " << tot << endl;
+  long long v;
+  vector<long long> masses;
+  while (fin >> v)
+    masses.push_back(v);
+  cout << "Part 1: " << getFuelPart1(masses) << endl;
+  cout << "Part 2: " << getFuelPart2(masses) << endl;
   return 0;
 }
