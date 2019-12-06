2414062387e9114fbb001945035a0018b46ab3a1
Joao Azevedo <joao.c.azevedo@gmail.com>
2019-12-06 11:26:34 +0000

    Add solution for second part of day 6

--- src/day06.cpp
+++ src/day06.cpp
@@ -42,11 +42,49 @@ int countOrbits(const map<string, vector<string> >& graph) {
   return tot;
 }
 
-int main() {
-  vector<pair<string, string> > orbits = readInput();
+map<string, vector<string> > buildOrbitGraph(vector<pair<string, string> > orbits) {
   map<string, vector<string> > graph;
   for (pair<string, string> orbit : orbits)
     graph[orbit.second].push_back(orbit.first);
+  return graph;
+}
+
+map<string, vector<string> > buildBidirectionalGraph(vector<pair<string, string> > orbits) {
+  map<string, vector<string> > graph;
+  for (pair<string, string> orbit : orbits) {
+    graph[orbit.second].push_back(orbit.first);
+    graph[orbit.first].push_back(orbit.second);
+  }
+  return graph;
+}
+
+int getDistance(const map<string, vector<string> >& graph, string start, string end) {
+  string s = graph.at(start).front();
+  string e = graph.at(end).front();
+  map<string, int> dist;
+  queue<string> q;
+  q.push(s);
+  dist[s] = 0;
+  while (!q.empty()) {
+    string curr = q.front(); q.pop();
+    vector<string> nextPlanets = graph.at(curr);
+    for (string next : nextPlanets) {
+      if (dist.find(next) == dist.end()) {
+        if (next == e)
+          return dist[curr] + 1;
+        dist[next] = dist[curr] + 1;
+        q.push(next);
+      }
+    }
+  }
+  return dist[e];
+}
+
+int main() {
+  vector<pair<string, string> > orbits = readInput();
+  map<string, vector<string> > graph = buildOrbitGraph(orbits);
   cout << "Part 1: " << countOrbits(graph) << endl;
+  map<string, vector<string> > completeGraph = buildBidirectionalGraph(orbits);
+  cout << "Part 2: " << getDistance(completeGraph, "YOU", "SAN") << endl;
   return 0;
 }
