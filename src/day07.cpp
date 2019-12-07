9cf2ff4ac27489362923c0fd64debdd3b0a36786
Joao Azevedo <joao.c.azevedo@gmail.com>
2019-12-07 15:27:52 +0000

    Add solution for second part of day 7

--- src/day07.cpp
+++ src/day07.cpp
@@ -40,10 +40,10 @@ bool validInstruction(int instruction) {
   return false;
 }
 
-vector<int> run(map<int, int> program, const vector<int>& input) {
-  vector<int> output, modes, params;
+tuple<bool, int> run(map<int, int>& program, queue<int>& input, int& intP) {
+  vector<int> modes, params;
   bool running = true;
-  int intP = 0, inputP = 0, p1, p2, p3;
+  int p1, p2, p3;
   while (running) {
     vector<int> modes;
     int instruction = program[intP] % 100;
@@ -69,16 +69,15 @@ vector<int> run(map<int, int> program, const vector<int>& input) {
 
       case 3:
         p1 = program[intP + 1];
-        program[p1] = input[inputP++];
+        program[p1] = input.front(); input.pop();
         intP += 2;
         break;
 
       case 4:
         modes = getModes(program[intP], 1);
         p1 = getValue(program, program[intP + 1], modes[0]);
-        output.push_back(p1);
         intP += 2;
-        break;
+        return make_tuple(running, p1);
 
       case 5:
         modes = getModes(program[intP], 2);
@@ -123,6 +122,18 @@ vector<int> run(map<int, int> program, const vector<int>& input) {
         break;
     }
   }
+  return make_tuple(running, -1);
+}
+
+vector<int> runContinuously(map<int, int> program, queue<int> input) {
+  vector<int> output;
+  int intP = 0;
+  while (true) {
+    auto res = run(program, input, intP);
+    if (!get<0>(res))
+      break;
+    output.push_back(get<1>(res));
+  }
   return output;
 }
 
@@ -133,8 +144,10 @@ int getBestOutputAux(const map<int, int>& program, int thrusters, int input, set
   for (int i = 0; i <= 4; ++i) {
     if (used.find(i) == used.end()) {
       used.insert(i);
-      vector<int> in = {i, input};
-      vector<int> out = run(program, in);
+      queue<int> in;
+      in.push(i);
+      in.push(input);
+      vector<int> out = runContinuously(program, in);
       assert(out.size() > 0);
       best = max(best, getBestOutputAux(program, thrusters - 1, out.back(), used));
       used.erase(i);
@@ -143,13 +156,53 @@ int getBestOutputAux(const map<int, int>& program, int thrusters, int input, set
   return best;
 }
 
-int getBestOutput(const map<int, int>& program, int thrusters) {
-  set<int> used = set<int>();
-  return getBestOutputAux(program, thrusters, 0, used);
+int getBestOutput(const map<int, int>& program) {
+  set<int> used;
+  return getBestOutputAux(program, 5, 0, used);
+}
+
+int getBestOutputLoopAux(const map<int, int>& program, int thrusters, vector<int>& signals, set<int>& used) {
+  if (thrusters == 0) {
+    vector<map<int, int> > programs = vector<map<int, int> >(5, program);
+    vector<queue<int> > inputs = vector<queue<int> >(5, queue<int>());
+    vector<int> inputP = vector<int>(5, 0);
+    for (size_t i = 0; i < signals.size(); ++i)
+      inputs[i].push(signals[i]);
+    inputs[0].push(0);
+    int currentP = 0;
+    int output = 0;
+    while (true) {
+      auto res = run(programs[currentP], inputs[currentP], inputP[currentP]);
+      if (!get<0>(res)) {
+        return output;
+      }
+      currentP = (currentP + 1) % 5;
+      output = get<1>(res);
+      inputs[currentP].push(output);
+    }
+  }
+  int best = 0;
+  for (int i = 5; i <= 9; ++i) {
+    if (used.find(i) == used.end()) {
+      used.insert(i);
+      signals.push_back(i);
+      best = max(best, getBestOutputLoopAux(program, thrusters - 1, signals, used));
+      signals.pop_back();
+      used.erase(i);
+    }
+  }
+  return best;
+}
+
+int getBestOutputLoop(const map<int, int>& program) {
+  set<int> used;
+  vector<int> signals;
+  return getBestOutputLoopAux(program, 5, signals, used);
 }
 
 int main() {
   map<int, int> program = readInput("day07.in");
-  cout << "Part 1: " << getBestOutput(program, 5) << endl;
+  cout << "Part 1: " << getBestOutput(program) << endl;
+  cout << "Part 2: " << getBestOutputLoop(program) << endl;
   return 0;
 }
