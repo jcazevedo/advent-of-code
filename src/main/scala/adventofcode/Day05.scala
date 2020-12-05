c5710556c76f15de5d272f1683f3c4c8fb357910
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-12-05 11:51:23 +0000

    Silence warnings

--- src/main/scala/adventofcode/Day05.scala
+++ src/main/scala/adventofcode/Day05.scala
@@ -1,16 +1,16 @@
 package adventofcode
 
 class Day05 extends DailyChallenge[Int, Int] {
-  def seatId(str: String): Int = {
-    str.foldLeft(0) {
-      case (curr, 'F' | 'L') => curr * 2 + 0
-      case (curr, 'B' | 'R') => curr * 2 + 1
+  def seatId(str: String): Int =
+    str.foldLeft(0) { case (curr, ch) =>
+      (ch: @unchecked) match {
+        case 'F' | 'L' => curr * 2 + 0
+        case 'B' | 'R' => curr * 2 + 1
+      }
     }
-  }
 
   def run(input: String): (Int, Int) = {
-    val seats = input.split("\n").toList
-    val seatIds = seats.map(seatId).toSet
+    val seatIds = input.split("\n").map(seatId).toSet
     (seatIds.max, (seatIds.min to seatIds.max).find(!seatIds(_)).get)
   }
 }
