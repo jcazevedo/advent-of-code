6b1f4acfad05ae9df5a3fa8bc7b5b6a5e225f058
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-12-05 17:47:11 +0000

    Make stuff smaller

--- src/main/scala/adventofcode/Day05.scala
+++ src/main/scala/adventofcode/Day05.scala
@@ -2,12 +2,7 @@ package adventofcode
 
 class Day05 extends DailyChallenge[Int, Int] {
   def seatId(str: String): Int =
-    str.foldLeft(0) { case (curr, ch) =>
-      (ch: @unchecked) match {
-        case 'F' | 'L' => curr * 2 + 0
-        case 'B' | 'R' => curr * 2 + 1
-      }
-    }
+    str.foldLeft(0) { case (curr, ch) => curr * 2 + (if (ch == 'B' || ch == 'R') 1 else 0) }
 
   def run(input: String): (Int, Int) = {
     val seatIds = input.split("\n").map(seatId).toSet
