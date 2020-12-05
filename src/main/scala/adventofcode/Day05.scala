2d7d2c208a99d58eb83eec2f5cb533e6baed6902
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-12-05 11:45:05 +0000

    Simplify solution for day 5 slightly

--- src/main/scala/adventofcode/Day05.scala
+++ src/main/scala/adventofcode/Day05.scala
@@ -1,21 +1,11 @@
 package adventofcode
 
 class Day05 extends DailyChallenge[Int, Int] {
-  def seat(str: String): (Int, Int) = {
-    val row = str.take(7).foldLeft(0) {
-      case (curr, 'F') => curr * 2 + 0
-      case (curr, 'B') => curr * 2 + 1
-    }
-    val col = str.drop(7).foldLeft(0) {
-      case (curr, 'L') => curr * 2 + 0
-      case (curr, 'R') => curr * 2 + 1
-    }
-    (row, col)
-  }
-
   def seatId(str: String): Int = {
-    val (row, col) = seat(str)
-    row * 8 + col
+    str.foldLeft(0) {
+      case (curr, 'F' | 'L') => curr * 2 + 0
+      case (curr, 'B' | 'R') => curr * 2 + 1
+    }
   }
 
   def run(input: String): (Int, Int) = {
