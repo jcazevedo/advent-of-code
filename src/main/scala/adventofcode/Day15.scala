29b8b8fdaf2296126d8f88ec6ddfa785ef2c3a07
Joao Azevedo <joao.c.azevedo@gmail.com>
2020-12-15 09:45:04 +0000

    Add solution for day 15

---
+++ src/main/scala/adventofcode/Day15.scala
@@ -0,0 +1,30 @@
+package adventofcode
+
+import scala.annotation.tailrec
+
+class Day15 extends DailyChallenge[Int, Int] {
+  @tailrec
+  private def go(
+      target: Int,
+      numbers: Vector[Int],
+      n: Int = 1,
+      lastSpoken: Int = -1,
+      cache: Map[Int, Int] = Map.empty
+  ): Int = {
+    val toSpeak =
+      if (n <= numbers.length) numbers(n - 1)
+      else if (!cache.contains(lastSpoken)) 0
+      else n - cache(lastSpoken) - 1
+
+    if (n == target) toSpeak else go(target, numbers, n + 1, toSpeak, cache.updated(lastSpoken, n - 1))
+  }
+
+  def part1(numbers: Vector[Int]): Int = go(2020, numbers)
+
+  def part2(numbers: Vector[Int]): Int = go(30000000, numbers)
+
+  def run(input: String): (Int, Int) = {
+    val numbers = input.split(",").map(_.trim.toInt).toVector
+    (part1(numbers), part2(numbers))
+  }
+}
