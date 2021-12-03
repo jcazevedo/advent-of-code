90126573f8f9848cadc9325ae4fc6284a0455ab3
Joao Azevedo <joao.c.azevedo@gmail.com>
2021-12-03 00:05:30 +0000

    Reformat source

--- day02-elm/Main.elm
+++ day02-elm/Main.elm
@@ -44,38 +44,34 @@ mult status = status.horizontalPosition * status.depth
 initialStatus : Status
 initialStatus = Status 0 0 0
 
-type alias InputType = String
-type alias OutputType = String
-
-port get : (InputType -> msg) -> Sub msg
-port put : OutputType -> Cmd msg
+port get : (String -> msg) -> Sub msg
+port put : String -> Cmd msg
 
 main : Program Flags Model Msg
 main =
-    Platform.worker
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        }
+  Platform.worker
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    }
 
 type alias Model = ()
 type Msg = Input String
 type alias Flags = ()
-
 
 init : Flags -> ( Model, Cmd Msg )
 init _ = ( (), Cmd.none )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        Input input -> ( model, put (run input))
+  case msg of
+    Input input -> ( model, put (run input))
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    get Input
+  get Input
 
-run : InputType -> OutputType
+run : String -> String
 run input =
   let
     commands = toCommands input
