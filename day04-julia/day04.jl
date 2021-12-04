7f7b1106c62f25a681793dcdbb103b0c12ff8bd8
Joao Azevedo <joao.c.azevedo@gmail.com>
2021-12-04 14:22:06 +0000

    Simplify solution for day 4

--- day04-julia/day04.jl
+++ day04-julia/day04.jl
@@ -1,17 +1,17 @@
 input_string = open(f->read(f, String), "inputs/04.input")
-input_lines = split(input_string, '\n', keepempty = false)
+input_lines = split(input_string, '\n', keepempty=false)
 numbers = map(x -> parse(Int64, x), split(input_lines[1], ','))
 
 function build_boards(n)
-  ans = Vector{Vector{Vector{Int64}}}()
+  ans = Vector{Matrix}()
   curr = Vector{Vector{Int64}}()
   curr_idx = 0
   for line in 2:length(input_lines)
-    row = map(x -> parse(Int64, x), split(input_lines[line], r"\s+", keepempty = false))
+    row = map(x -> parse(Int64, x), split(input_lines[line], r"\s+", keepempty=false))
     push!(curr, row)
     curr_idx = curr_idx + 1
     if curr_idx == n
-      push!(ans, curr)
+      push!(ans, hcat(curr...))
       curr = Vector{Vector{Int64}}()
       curr_idx = 0
     end
@@ -22,59 +22,17 @@ end
 boards = build_boards(5)
 
 function draw(board, number)
-  n = length(board)
-  for i in 1:n
-    for j in 1:n
-      if board[i][j] == number
-        board[i][j] = 0
-      end
-    end
-  end
+  map(row -> map(num -> if num == number 0 else num end, row), board)
 end
 
 function wins(board)
-  n = length(board)
-  has_row = false
-  for i in 1:n
-    tmp = true
-    for j in 1:n
-      if board[i][j] != 0
-        tmp = false
-      end
-    end
-    if tmp
-      has_row = true
-      break
-    end
-  end
-  if has_row
-    return has_row
-  end
-  has_col = false
-  for j in 1:n
-    tmp = true
-    for i in 1:n
-      if board[i][j] != 0
-        tmp = false
-      end
-    end
-    if tmp
-      has_col = true
-      break
-    end
-  end
-  return has_col
+  row_sums = sum(board, dims=1)
+  col_sums = sum(board, dims=2)
+  return length(filter(x -> x == 0, col_sums)) > 0 || length(filter(x -> x == 0, row_sums)) > 0
 end
 
 function sum_unmarked(board)
-  ans = 0
-  n = length(board)
-  for row in board
-    for num in row
-      ans = ans + num
-    end
-  end
-  ans
+  sum(map(row -> sum(row), board))
 end
 
 function go()
@@ -90,7 +48,7 @@ function go()
       if won[i] == 1
         continue
       end
-      draw(boards[i], draw_number)
+      boards[i] = draw(boards[i], draw_number)
       if wins(boards[i])
         won[i] = 1
         total_won = total_won + 1
