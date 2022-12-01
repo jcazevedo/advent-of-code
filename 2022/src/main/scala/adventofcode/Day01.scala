a39ac2e9712949285fb4a0176f75c338372ecc3d
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-12-01 22:49:37 +0000

    Add solution for day 1

---
+++ 2022/src/main/scala/adventofcode/Day01.scala
@@ -0,0 +1,16 @@
+package adventofcode
+
+object Day01 extends DailyChallenge[Int, Int] {
+  def run(input: String): (Int, Int) = {
+    val caloriesCarried = input.split("\n").foldRight(List.empty[Int]) {
+      case (str, Nil)                      => List(str.toInt)
+      case (str, curr) if str.trim.isEmpty => 0 :: curr
+      case (str, h :: t)                   => (h + str.toInt) :: t
+    }
+
+    val part1 = caloriesCarried.max
+    val part2 = caloriesCarried.sorted.takeRight(3).sum
+
+    (part1, part2)
+  }
+}
