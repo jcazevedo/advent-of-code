69cdd74a3ae12367812d348ac91b02e45c83f1cd
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-12-03 11:39:32 +0000

    Reuse some stuff

--- 2022/src/main/scala/Day03.scala
+++ 2022/src/main/scala/Day03.scala
@@ -3,19 +3,19 @@ object Day03 extends DailyChallenge[Int, Int] {
     if (item <= 'Z') (item - 'A') + 27
     else (item - 'a') + 1
 
-  def repeatedItem(rucksack: String): Char = {
+  def split(rucksack: String): List[String] = {
     val (first, second) = rucksack.splitAt(rucksack.length / 2)
-    (first.toSet & second.toSet).head
+    List(first, second)
   }
 
-  def badge(rucksacks: List[String]): Char =
+  def repeated(rucksacks: List[String]): Char =
     rucksacks.map(_.toSet).reduce(_ & _).head
 
   def run(input: String): (Int, Int) = {
     val rucksacks = input.split("\n").toList
 
-    val part1 = rucksacks.map(repeatedItem).map(priority).sum
-    val part2 = rucksacks.sliding(3, 3).map(badge).map(priority).sum
+    val part1 = rucksacks.map(split).map(repeated).map(priority).sum
+    val part2 = rucksacks.sliding(3, 3).map(repeated).map(priority).sum
 
     (part1, part2)
   }
