cf3c66c730997cc2b1b6f9dc1c21409ca328f5ad
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-12-04 12:32:48 +0000

    Reuse more stuff

--- 2021/scala/src/main/scala/Day14.scala
+++ 2021/scala/src/main/scala/Day14.scala
@@ -1,21 +1,6 @@
 import scala.collection.mutable
 
-object Day14 extends DailyChallenge[Int, Long] {
-  def step(polymer: String, rules: Map[String, String]): String = {
-    val res = mutable.StringBuilder()
-
-    polymer.indices.foreach { idx =>
-      res += polymer(idx)
-      if (idx + 1 < polymer.length)
-        res.append(rules.getOrElse(s"${polymer(idx)}${polymer(idx + 1)}", ""))
-    }
-
-    res.toString
-  }
-
-  def elementCounts(polymer: String): Map[Char, Int] =
-    polymer.groupBy(identity).view.mapValues(_.size).toMap
-
+object Day14 extends DailyChallenge[Long, Long] {
   def finalCountOf(ch: Char, polymer: String, after: Int, rules: Map[String, String]): Long = {
     val cache = mutable.Map.empty[(String, Int), Long]
 
@@ -39,7 +24,7 @@ object Day14 extends DailyChallenge[Int, Long] {
     aux(polymer, after)
   }
 
-  def run(input: String): (Int, Long) = {
+  def run(input: String): (Long, Long) = {
     val lines = input.split("\n").map(_.trim).toList
 
     val polymer = lines.head
@@ -51,13 +36,13 @@ object Day14 extends DailyChallenge[Int, Long] {
       })
       .toMap
 
-    val after10Steps = (1 to 10).foldLeft(polymer)((curr, _) => step(curr, rules))
-    val cnts = elementCounts(after10Steps)
-    val part1 = cnts.values.max - cnts.values.min
-
     val validElements = polymer.toSet ++ rules.keySet.flatMap(_.toSet) ++ rules.values.flatMap(_.toSet).toSet
-    val part2Cnts = validElements.toList.map(element => element -> finalCountOf(element, polymer, 40, rules)).toMap
-    val part2 = part2Cnts.values.max - part2Cnts.values.min
+
+    val cnts1 = validElements.toList.map(element => element -> finalCountOf(element, polymer, 10, rules)).toMap
+    val part1 = cnts1.values.max - cnts1.values.min
+
+    val cnts2 = validElements.toList.map(element => element -> finalCountOf(element, polymer, 40, rules)).toMap
+    val part2 = cnts2.values.max - cnts2.values.min
 
     (part1, part2)
   }
