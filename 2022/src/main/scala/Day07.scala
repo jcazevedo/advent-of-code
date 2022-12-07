e811f6232faf5889ced0b3d49b42b2a3c9456b2c
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-12-07 11:15:52 +0000

    Apply minor style fixes

--- 2022/src/main/scala/Day07.scala
+++ 2022/src/main/scala/Day07.scala
@@ -1,18 +1,24 @@
 object Day07 extends DailyChallenge[Long, Long] {
   sealed trait Node
+
+  case class File(size: Long) extends Node
+
   case class Directory(contained: Map[String, Node] = Map.empty) extends Node {
     def add(node: Node, path: List[String]): Directory =
-      add(node, path.head, path.tail)
+      path match {
+        case Nil =>
+          this
 
-    def add(node: Node, name: String, rest: List[String] = List.empty): Directory =
-      if (rest.isEmpty) copy(contained = contained + (name -> node))
-      else
-        copy(contained = contained + (name -> (contained.get(name) match {
-          case Some(d: Directory) => d.add(node, rest.head, rest.tail)
-          case _                  => Directory().add(node, rest.head, rest.tail)
-        })))
+        case h :: Nil =>
+          copy(contained = contained + (h -> node))
+
+        case h :: t =>
+          copy(contained = contained + (h -> (contained.get(h) match {
+            case Some(d: Directory) => d.add(node, t)
+            case _                  => Directory().add(node, t)
+          })))
+      }
   }
-  case class File(size: Long) extends Node
 
   def buildTree(input: List[String]): Directory = {
     var ans = Directory()
@@ -20,23 +26,25 @@ object Day07 extends DailyChallenge[Long, Long] {
     def aux(commands: List[String], directories: List[String]): Unit = {
       if (commands.nonEmpty) {
         val currCommand = commands.head.substring(2, 4)
-        if (currCommand == "cd") {
-          val target = commands.head.drop(5)
-          if (target == "/") aux(commands.tail, directories = List.empty)
-          else if (target == "..") aux(commands.tail, directories.tail)
-          else aux(commands.tail, target :: directories)
-        } else if (currCommand == "ls") {
-          val output = commands.tail.takeWhile(!_.startsWith("$"))
-          output.foreach { line =>
-            if (line.startsWith("dir")) {
-              val name = line.stripPrefix("dir ")
-              ans = ans.add(Directory(), (name :: directories).reverse)
-            } else {
-              val Array(size, name) = line.split("\\s+")
-              ans = ans.add(File(size.toLong), (name :: directories).reverse)
-            }
-          }
-          aux(commands.tail.dropWhile(!_.startsWith("$")), directories)
+        currCommand match {
+          case "cd" =>
+            val target = commands.head.drop(5)
+            if (target == "/") aux(commands.tail, directories = List.empty)
+            else if (target == "..") aux(commands.tail, directories.tail)
+            else aux(commands.tail, target :: directories)
+
+          case "ls" =>
+            val output = commands.tail.takeWhile(!_.startsWith("$"))
+            output.foreach(line =>
+              if (line.startsWith("dir")) {
+                val name = line.stripPrefix("dir ")
+                ans = ans.add(Directory(), (name :: directories).reverse)
+              } else {
+                val Array(size, name) = line.split("\\s+")
+                ans = ans.add(File(size.toLong), (name :: directories).reverse)
+              }
+            )
+            aux(commands.tail.drop(output.length), directories)
         }
       }
     }
@@ -56,20 +64,19 @@ object Day07 extends DailyChallenge[Long, Long] {
     n match {
       case d @ Directory(contained) =>
         Map(curr.toList -> totalSize(d)) ++ contained.flatMap({ case (k, v) => getDirSizes(v, curr :+ k) })
-      case _ => Map.empty
+
+      case _ =>
+        Map.empty
     }
 
   def run(input: String): (Long, Long) = {
     val lines = input.split("\n").map(_.trim).toList
 
     val baseDir = buildTree(lines)
-    val totalSpace = 70000000L
-    val unusedNeeded = 30000000L
     val usedSpace = totalSize(baseDir)
-    val target = totalSpace - unusedNeeded
 
-    val part1 = getDirSizes(baseDir).filter(_._2 < 100000).values.sum
-    val part2 = getDirSizes(baseDir).filter(usedSpace - _._2 < target).values.toList.sorted.head
+    val part1 = getDirSizes(baseDir).filter(_._2 < 100000L).values.sum
+    val part2 = getDirSizes(baseDir).filter(usedSpace - _._2 < 40000000L).values.toList.min
 
     (part1, part2)
   }
