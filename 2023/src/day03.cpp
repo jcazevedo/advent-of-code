80a6e2a87f2b6d59b203eaaa376e5b2317f1c32a
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-12-03 12:14:02 +0000

    Avoid the visited vector

--- 2023/src/day03.cpp
+++ 2023/src/day03.cpp
@@ -2,14 +2,17 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <tuple>
 #include <vector>
 
 using namespace std;
 
-tuple<int, int> solve(const vector<string>& grid) {
+int main() {
+  vector<string> grid;
+  ifstream fin("input/day03.in");
+  string s;
+  while (fin >> s) { grid.push_back(s); }
+  fin.close();
   int M = grid.size(), N = grid[0].size(), part1 = 0, part2 = 0;
-  vector<vector<bool>> visited(M, vector<bool>(N, false));
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
       if (isdigit(grid[i][j]) || grid[i][j] == '.') { continue; }
@@ -19,14 +22,12 @@ tuple<int, int> solve(const vector<string>& grid) {
         if (ni < 0 || ni >= M) { continue; }
         for (int dj = -1; dj <= 1; ++dj) {
           int nj = j + dj;
-          if (nj < 0 || nj >= N || !isdigit(grid[ni][nj]) || visited[ni][nj]) {
-            continue;
-          }
+          if (nj < 0 || nj >= N || !isdigit(grid[ni][nj])) { continue; }
           while (nj >= 0 && isdigit(grid[ni][nj])) { nj--; }
           int num = 0;
           while (++nj < N && isdigit(grid[ni][nj])) {
-            visited[ni][nj] = true;
             num = num * 10 + grid[ni][nj] - '0';
+            grid[ni][nj] = '.';
           }
           part1 += num;
           if (grid[i][j] == '*') {
@@ -38,17 +39,6 @@ tuple<int, int> solve(const vector<string>& grid) {
       if (gearNumbers == 2) { part2 += gear; }
     }
   }
-  return {part1, part2};
-}
-
-int main() {
-  vector<string> grid;
-  ifstream fin("input/day03.in");
-  string s;
-  while (fin >> s) { grid.push_back(s); }
-  fin.close();
-  int part1, part2;
-  tie(part1, part2) = solve(grid);
   cout << "Part 1: " << part1 << endl;
   cout << "Part 2: " << part2 << endl;
 }
