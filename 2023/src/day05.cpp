29468fe51b47cc6045e2d239fbebc22b0a85078d
Joao Azevedo <joao.c.azevedo@gmail.com>
2023-12-06 00:22:36 +0000

    Add solution to day 5

---
+++ 2023/src/day05.cpp
@@ -0,0 +1,134 @@
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <sstream>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+struct Range {
+  long long destination;
+  long long source;
+  long long length;
+};
+
+vector<long long> seeds;
+map<tuple<string, string>, vector<Range>> ranges;
+
+long long find(string from, string to, long long idx) {
+  if (from == to) { return idx; }
+  for (auto itr = ranges.begin(); itr != ranges.end(); ++itr) {
+    if (get<0>(itr->first) == from) {
+      for (const Range& range : itr->second) {
+        if (idx >= range.source && idx < range.source + range.length) {
+          return find(get<1>(itr->first), to,
+                      range.destination + idx - range.source);
+        }
+      }
+      return find(get<1>(itr->first), to, idx);
+    }
+  }
+  return -1;
+}
+
+long long part1() {
+  long long ans = -1;
+  for (long long seed : seeds) {
+    long long location = find("seed", "location", seed);
+    if (ans == -1 || location < ans) { ans = location; }
+  }
+  return ans;
+}
+
+long long lowestValue(string from,
+                      string to,
+                      long long source,
+                      long long length) {
+  if (from == to) { return source; }
+  long long ans = numeric_limits<long>::max();
+  for (auto itr = ranges.begin(); itr != ranges.end(); ++itr) {
+    if (get<0>(itr->first) == from) {
+      while (length != 0) {
+        bool used = false;
+        for (const Range& range : itr->second) {
+          if (length == 0) { break; }
+          if (source >= range.source && source < range.source + range.length) {
+            long long rangeLength =
+                min(range.length - (source - range.source), length);
+            ans =
+                min(ans, lowestValue(get<1>(itr->first), to,
+                                     range.destination + source - range.source,
+                                     rangeLength));
+            length -= rangeLength;
+            source += rangeLength;
+            used = true;
+            break;
+          }
+        }
+        if (!used && length > 0) {
+          long long minSource = numeric_limits<long>::max();
+          long long maxSource = numeric_limits<long>::min();
+          for (const Range& range : itr->second) {
+            if (range.source > source) {
+              minSource = min(minSource, range.source);
+            }
+            maxSource = max(maxSource, range.source + range.length);
+          }
+          if (source < minSource) {
+            long long rangeLength = min(minSource - source, length);
+            ans = min(ans,
+                      lowestValue(get<1>(itr->first), to, source, rangeLength));
+            source += rangeLength;
+            length -= rangeLength;
+          } else if (source >= maxSource) {
+            ans = min(ans, lowestValue(get<1>(itr->first), to, source, length));
+            length = 0;
+          }
+        }
+      }
+      break;
+    }
+  }
+  return ans;
+}
+
+long long part2() {
+  long long ans = numeric_limits<long>::max();
+  int N = seeds.size();
+  for (int i = 0; i < N; i += 2) {
+    ans = min(ans, lowestValue("seed", "location", seeds[i], seeds[i + 1]));
+  }
+  return ans;
+}
+
+int main() {
+  ifstream fin("input/day05.in");
+  string s, tmp;
+  getline(fin, s);
+  istringstream ss(s);
+  ss >> tmp;
+  long long seed;
+  while (ss >> seed) { seeds.push_back(seed); }
+  getline(fin, s);
+  tuple<string, string> curr;
+  while (getline(fin, s)) {
+    if (s.size() == 0) { continue; }
+    if (isdigit(s[0])) {
+      Range r;
+      istringstream ss(s);
+      ss >> r.destination >> r.source >> r.length;
+      ranges[curr].push_back(r);
+    } else {
+      size_t toLocation = s.find("-to-");
+      string from = s.substr(0, toLocation);
+      string to = s.substr(toLocation + 4, s.find(" ") - toLocation - 4);
+      curr = {from, to};
+    }
+  }
+  fin.close();
+  cout << "Part 1: " << part1() << endl;
+  cout << "Part 2: " << part2() << endl;
+  return 0;
+}
