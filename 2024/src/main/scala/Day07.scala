6e3abbf1a303f366b1a60315c40ac74297d3b64d
Joao Azevedo <joao.c.azevedo@gmail.com>
2024-12-07 12:17:38 +0000

    Add solution to day 7

---
+++ 2024/src/main/scala/Day07.scala
@@ -0,0 +1,32 @@
+object Day07 extends DailyChallenge[Long, Long] {
+  case class Equation(testValue: Long, numbers: List[Long])
+
+  def isTrue(equation: Equation, operations: List[(Long, Long) => Long]): Boolean = {
+    def isTrueAux(target: Long, curr: Long, remaining: List[Long]): Boolean =
+      remaining match {
+        case Nil    => target == curr
+        case h :: t => operations.exists(op => isTrueAux(target, op(curr, h), t))
+      }
+
+    isTrueAux(equation.testValue, equation.numbers.head, equation.numbers.tail)
+  }
+
+  def run(input: String): (Long, Long) = {
+    val equations = input
+      .split("\n")
+      .map({ case s"$testValue: $numbers" =>
+        Equation(testValue.toLong, numbers.split("\\s+").map(_.toLong).toList)
+      })
+
+    val part1 = equations
+      .filter(isTrue(_, operations = List(_ + _, _ * _)))
+      .map(_.testValue)
+      .sum
+    val part2 = equations
+      .filter(isTrue(_, operations = List(_ + _, _ * _, (v1, v2) => (v1.toString ++ v2.toString).toLong)))
+      .map(_.testValue)
+      .sum
+
+    (part1, part2)
+  }
+}
